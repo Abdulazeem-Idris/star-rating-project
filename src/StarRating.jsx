3de533be13eaafda94bb7c572a34877cdc073688
Abdulazeem-Idris <omofolahan.19@gmail.com>
2023-09-09 00:53:06 +0100

    changed from import './utilis' to import from react

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -1,4 +1,4 @@
-import { range } from './utils';
+/*import { range } from './utils';
 
 function StarRating({ rating }) {
   // Define a variable for the star element
@@ -18,4 +18,27 @@ function StarRating({ rating }) {
   return <div className="star-wrapper">{stars}</div>;
 }
 
+export default StarRating;*/
+
+import React from 'react';
+
+function StarRating({ rating }) {
+  // Create an array of stars based on the rating prop
+  const stars = [];
+
+  for (let i = 0; i < rating; i++) {
+    stars.push(
+      <img
+        key={i}
+        alt=""
+        className="gold-star"
+        src="https://sandpack-bundler.vercel.app/img/gold-star.svg"
+      />
+    );
+  }
+
+  return <div className="star-wrapper">{stars}</div>;
+}
+
 export default StarRating;
+
